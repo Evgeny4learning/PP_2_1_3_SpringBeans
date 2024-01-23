22847647c4597778f8e8302c22632b33e8cdf241
Evgeny4learning <fulln1ghtt@yandex.ru>
2024-01-23 14:47:13 +0300

    added Dog, solution

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,21 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+    private final Animal animal;
+    private final Timer time;
+
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer time) {
+        this.animal = animal;
+        this.time = time;
+    }
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +23,9 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return time;
     }
 }
