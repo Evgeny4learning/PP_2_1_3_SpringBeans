679116193218cea81f53a93f88e56055b718bdce
Evgeny4learning <fulln1ghtt@yandex.ru>
2024-01-30 13:17:39 +0300

    @Autowired for fields

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,21 +7,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
-    private final Animal animal;
-    private final Timer time;
+    @Qualifier("dog")
+    @Autowired
+    private Animal animal;
 
     @Autowired
-    public AnimalsCage(@Qualifier("dog") Animal animal, Timer time) {
-        this.animal = animal;
-        this.time = time;
-    }
+    private Timer time;
+
 
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(time.getTime());
         System.out.println("________________________");
     }
 
